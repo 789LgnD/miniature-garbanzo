2ae761f30201db82bf149c8047e0ba68bda3f300
789LgnD <165673036+789LgnD@users.noreply.github.com>
2024-04-01 23:35:51 +1000

    jyrjryjyrryjrryjrjjrjrjyrjrjr

--- wattt.py
+++
@@ -1,8 +0,0 @@
-from datetime import date
-current_date = date.today
-import datetime
-current_date_time = datetime.datetime.now()
-current_time = current_date_time.time()
-from datetime import datetime
-now = "понедельник"
-print ("сегодня", now, "сейчас", current_time, "дата", current_date())
