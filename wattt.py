b191aa2a804a194a62b0731440c8cdb2b918a8ad
789LgnD <165673036+789LgnD@users.noreply.github.com>
2024-04-01 23:30:05 +1000

    Add files via upload

---
+++ wattt.py
@@ -0,0 +1,8 @@
+from datetime import date
+current_date = date.today
+import datetime
+current_date_time = datetime.datetime.now()
+current_time = current_date_time.time()
+from datetime import datetime
+now = "понедельник"
+print ("сегодня", now, "сейчас", current_time, "дата", current_date())
